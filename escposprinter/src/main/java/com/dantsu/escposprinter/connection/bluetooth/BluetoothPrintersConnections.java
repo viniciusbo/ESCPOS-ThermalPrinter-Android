82762c69b7a7c9c2b68b3b587582f3782dae9e61
Vinícius Borriello <viniciusbo@gmail.com>
2023-05-22 12:35:49 -0300

    Remove device class check before BT connection

--- escposprinter/src/main/java/com/dantsu/escposprinter/connection/bluetooth/BluetoothPrintersConnections.java
+++ escposprinter/src/main/java/com/dantsu/escposprinter/connection/bluetooth/BluetoothPrintersConnections.java
@@ -54,9 +54,9 @@ public class BluetoothPrintersConnections extends BluetoothConnections {
             int majDeviceCl = device.getBluetoothClass().getMajorDeviceClass(),
                     deviceCl = device.getBluetoothClass().getDeviceClass();
 
-            if (majDeviceCl == BluetoothClass.Device.Major.IMAGING && (deviceCl == 1664 || deviceCl == BluetoothClass.Device.Major.IMAGING)) {
+            // if (majDeviceCl == BluetoothClass.Device.Major.IMAGING && (deviceCl == 1664 || deviceCl == BluetoothClass.Device.Major.IMAGING)) {
                 printersTmp[i++] = new BluetoothConnection(device);
-            }
+            // }
         }
         BluetoothConnection[] bluetoothPrinters = new BluetoothConnection[i];
         System.arraycopy(printersTmp, 0, bluetoothPrinters, 0, i);
